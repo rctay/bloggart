4e3bb704e8cc2e3fde079d96f03cffaba3ce8bac
Tay Ray Chuan <rctay89@gmail.com>
2012-04-26 18:14:31 +0800

    forms.clean_data is broken

--- handlers.py
+++ handlers.py
@@ -91,7 +91,7 @@ class PostHandler(BaseHandler):
                     initial={'draft': post and post.published is None})
     if form.is_valid():
       post = form.save(commit=False)
-      if form.clean_data['draft']:# Draft post
+      if form.cleaned_data['draft']:# Draft post
         post.published = datetime.datetime.max
         post.put()
       else:
@@ -102,7 +102,7 @@ class PostHandler(BaseHandler):
         post.publish()
       self.render_to_response("published.html", {
           'post': post,
-          'draft': form.clean_data['draft']})
+          'draft': form.cleaned_data['draft']})
     else:
       self.render_form(form)
 
